5a306be28ff900a6953ad39d2c874f3b8e9f1418
Kiczu <adrian.mamyto@gmail.com>
2025-04-08 20:39:58 +0200

    feat: adding username to databox

--- src/views/UserDashboard/UserDataInfo/UserDataInfo.tsx
+++ src/views/UserDashboard/UserDataInfo/UserDataInfo.tsx
@@ -4,6 +4,7 @@ import { userDataInfoBox } from "./userDataInfo.style";
 
 const UserDataInfo = () => {
   const { userData } = useAuth();
+  console.log(userData);
 
   return (
     <Box>
@@ -11,7 +12,7 @@ const UserDataInfo = () => {
         Your Data
       </Typography>
       <Box sx={userDataInfoBox}>
-        {/* <Typography variant="body1">{userData?.username}</Typography> */}
+        <Typography variant="body1">Username: {userData?.username}</Typography>
         <Typography variant="body1">E-mail: {userData?.email}</Typography>
         <Typography variant="body1">Name: {userData?.firstName}</Typography>
         <Typography variant="body1">Surname: {userData?.lastName}</Typography>
