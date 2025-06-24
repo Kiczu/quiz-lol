52974b4ebf4e34fbfcf35425d8daf9afd1376dca
Kiczu <adrian.mamyto@gmail.com>
2025-06-24 21:59:07 +0200

    feat(theme): add BeaufortforLOL font and apply to headings

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -2,20 +2,23 @@ import { colors } from "./colors";
 
 export const typography = {
     fontFamily: "'Spiegel', sans-serif",
-    fontSize: 14,
+    fontSize: 18,
     h1: {
+        fontFamily: "'BeaufortforLOL', sans-serif",
         fontSize: "2.5rem",
         fontWeight: 400,
         lineHeight: 1.2,
         color: `${colors.textPrimary}`,
     },
     h2: {
+        fontFamily: "'BeaufortforLOL', sans-serif",
         fontSize: "2rem",
         fontWeight: 500,
         lineHeight: 1.3,
         color: `${colors.gold2}`,
     },
     h3: {
+        fontFamily: "'BeaufortforLOL', sans-serif",
         fontSize: "1.5rem",
         fontWeight: 500,
         lineHeight: 1.35,
@@ -34,6 +37,7 @@ export const typography = {
         color: `${colors.gold2}`,
     },
     button: {
+        fontFamily: "'BeaufortforLOL', sans-serif",
         fontSize: "0.875rem",
         fontWeight: 700,
         textTransform: "uppercase",
